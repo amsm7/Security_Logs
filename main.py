8e7c4cd7294227081dedcdf72b4f671e41e84c0f
amsm7 <49472469+amsm7@users.noreply.github.com>
2022-12-05 21:12:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from Menus import Menus
 import pandas as pd
 import numpy as np
 
-
 def run_program(df):
 
     input_obj = Menus()
@@ -21,17 +20,25 @@ def run_program(df):
 def creating_data_frame():
     """ Creating new Data frame with just column names. """
     data_frame = pd.DataFrame(
-        columns=['Host', 'Event Type', 'Total Events'])
+        columns=['Host', 'Event Type', 'Total Events', 'Date'])
 
-    """Creating 2 NumPy arrays"""
-    hosts = np.array(['host1', 'host2', 'host3', 'host4', 'host5', 'host6', 'host7', 'host8', 'host9', 'host10'])
+
+    """Creating 3 NumPy arrays"""
+    hosts = np.array(['host1', 'host2', 'host3', 'host4', 'host5',
+                      'host6', 'host7', 'host8', 'host9', 'host10',
+                      'host11', 'host12', 'host13', 'host14', 'host15',
+                      'host16', 'host17', 'host18', 'host19', 'host20',
+                      'host21', 'host22', 'host23', 'host24', 'host25'])
     event_type = np.array(['Wrong Password', 'Invalid User', "'Long' Login", None])
+    random_dates = np.array(pd.date_range('2022/11/01', '2022/12/03'))
 
     """Filling the data frame with different data types."""
     data_frame['Host'] = np.random.choice(hosts, 10000)
     data_frame['Event Type'] = np.random.choice(event_type, 10000)
     data_frame['Total Events'] = np.random.randint(0, 3000, 10000)
-    run_program(data_frame)
+    data_frame['Date'] = np.random.choice(random_dates, 10000)
+    df_copy = data_frame.copy()
+    run_program(df_copy)
 
 
 creating_data_frame()
